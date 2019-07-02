672f8d47bcf381caea3dd909dc21c0fa01ba0939
qianming2 <a1161108496@qq.com>
2019-07-02 15:15:30 +0800

    7

--- dome01/src/main/java/servlet/MyServlet.java
+++ dome01/src/main/java/servlet/MyServlet.java
@@ -18,7 +18,7 @@ public class MyServlet implements Servlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
       res.getWriter().write("hahahhsddsasadasda");
-        res.getWriter().write("hahahhsddsasadasda");
+        res.getWriter().write("hahahhsddsasadasdas");
     }
 
     @Override
