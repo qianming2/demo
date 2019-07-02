6c95ab2f87252f1bdd5592e51e380bd9504bbce2
qianming2 <a1161108496@qq.com>
2019-07-02 18:46:13 +0800

    第一次上传

---
+++ src/main/java/servlet/MyServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyServlet implements Servlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().write("ssssss");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
