3d803741a19abd31a661b99b0088ffaee79e1a2a
qianming2 <a1161108496@qq.com>
2019-07-02 18:59:51 +0800

    2

--- src/main/java/servlet/MyServlet.java
+++ src/main/java/servlet/MyServlet.java
@@ -17,8 +17,8 @@ public class MyServlet implements Servlet {
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        res.getWriter().write("sssssss");
-    }时
+        res.getWriter().write("sssssss删除");
+    }
 
     @Override
     public String getServletInfo() {
